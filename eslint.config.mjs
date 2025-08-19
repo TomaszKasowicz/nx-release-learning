3a333bb6a7a6c7ee734a24141f0689811aa79af2
Tomasz Kasowicz <tomaszk@backbase.com>
2025-08-19 15:47:48 +0200

    chore: add sherrif

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 import nx from '@nx/eslint-plugin';
+import sheriff from '@softarc/eslint-plugin-sheriff';
 
 export default [
   ...nx.configs['flat/base'],
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
+  sheriff.configs.all,
   {
     ignores: ['**/dist'],
   },
@@ -23,20 +25,6 @@ export default [
         },
       ],
     },
-  },
-  {
-    files: [
-      '**/*.ts',
-      '**/*.tsx',
-      '**/*.cts',
-      '**/*.mts',
-      '**/*.js',
-      '**/*.jsx',
-      '**/*.cjs',
-      '**/*.mjs',
-    ],
-    // Override or add rules here
-    rules: {},
   },
   {
     files: ['*.json'],
