af1b55c7e6bf432e088a587cfe54dd86d64ca43d
Tomasz Kasowicz <tomasz.kasowicz@gmail.com>
2025-08-20 15:34:32 +0200

    feat: test

--- libs/shared/src/lib/shared-journey-lib.ts
+++ libs/shared/src/lib/shared-journey-lib.ts
@@ -3,7 +3,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'lib-shared-journey-lib',
   imports: [],
-  template: `<p>SharedJourneyLib works! And has a new feature</p>`,
+  template: `<p>SharedJourneyLib works! And has a new feature1</p>`,
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
